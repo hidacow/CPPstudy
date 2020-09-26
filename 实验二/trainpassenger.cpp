59fb8355ab665d9b9cad238d7e2d71db0311b85c
hidacow <68262945+hidacow@users.noreply.github.com>
2020-09-26 11:32:52 +0800

    fix minor problem

--- 实验二/trainpassenger.cpp
+++ 实验二/trainpassenger.cpp
@@ -7,17 +7,16 @@ int main() {
     while (cin>>a>>n>>m>>x){//This problem require multiple input, need to detect EOF
 
 
-    if(x==1) {
-        cout<<a;//if require 1st stop then print a
-        //return 0;//exit program
-    }
-    //make a Fibonacci array
-    for(i=3;i<=17;i++){//Fibonacci rule start from item 3
-        fib[i]=fib[i-1]+fib[i-2];//execute Fibonacci rule
-    }
-    if(n>4){
-        k=(m-(fib[n-3]+1)*a)/(fib[n-2]-1);//find out fib(n-3)*a+fib(n-2)*k=m, solve k
-    }
-    cout<<(fib[x-2]+1)*a+(fib[x-1]-1)*k;
+        if(x==1) {
+            cout<<a;//if require 1st stop then print a
+        }
+        //make a Fibonacci array
+        for(i=3;i<=17;i++){//Fibonacci rule start from item 3
+            fib[i]=fib[i-1]+fib[i-2];//execute Fibonacci rule
+        }
+        if(n>4){
+            k=(m-(fib[n-3]+1)*a)/(fib[n-2]-1);//find out fib(n-3)*a+fib(n-2)*k=m, solve k
+        }
+        cout<<(fib[x-2]+1)*a+(fib[x-1]-1)*k<<endl;
     }
 }
