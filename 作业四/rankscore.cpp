e2e474247896515b9d18e7d13f8d5019fba5481f
hidacow <68262945+hidacow@users.noreply.github.com>
2020-10-23 10:59:22 +0800

    minor improvement

--- 作业四/rankscore.cpp
+++ 作业四/rankscore.cpp
@@ -19,16 +19,12 @@ int main() {
             reverse(sortedscore.begin(), sortedscore.end());
             int j = 0;
             int i = 0;
-            for (; i < score.size() - 1; i++) {
+            for (; i <= score.size() - 1; i++) {
                 j = 0;
                 while (sortedscore[j] != score[i])j++;
-                cout << j + 1 << ", ";  //search the score in the sorted array and print its rank
+                if(i != score.size() - 1)cout << j + 1 << ", ";  //search the score in the sorted array and print its rank
+                else cout << j + 1 << endl;  //the last one do not have ", " after it
             }
-
-            i = score.size() - 1;
-            j = 0;
-            while (sortedscore[j] != score[i])j++;
-            cout << j + 1 << endl;  //the last one do not have ", " after it
             score.clear();  //clear the array
             sortedscore.clear();
         }
